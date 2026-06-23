d4dd9218c2cb2f1eea96ef60c14b769c3d299ce4
JanisRUS <janisrus35@gmail.com>
2026-06-24 02:38:18 +0300

    Создан шаблон модуля lader

---
+++ src/lader.c
@@ -0,0 +1,9 @@
+/// @file       lader.c
+/// @brief      См. lader.h
+/// @author     Тузиков Г.А. janisrus35@gmail.com
+
+int lader(const char *inputFilePtr, const char *outputFilePtr)
+{
+    // TODO
+    return 0;
+}
